6887ee1c6f9d0e4dad2733112f60c56b0302c5b6
Alexey Guryev <alguryew@gmail.com>
2017-01-29 19:40:23 +0300

    React/redux simple example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts', pages: 200},
+        { title: 'Harry Potter', pages: 301},
+        { title: 'The Dark Tower', pages: 120},
+        { title: 'Eloquent Ruby', pages: 20}
+    ];
+}
